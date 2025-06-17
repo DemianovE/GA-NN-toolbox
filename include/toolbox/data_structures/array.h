f9875e1503e7d1ba17d0f691c15fcba7e85a7317
DemianovE <work.demianov@gmail.com>
2025-06-17 17:23:55 +0200

    Another Array refit THis is move to proper interface like approach 1) change name convention to DS_FunctionName to make it code visible and manageable 2) add proper documentation for object 3) add actions to the interface to make direct object actions obsolete

--- include/toolbox/data_structures/array.h
+++ include/toolbox/data_structures/array.h
@@ -1,7 +1,15 @@
+/**
+* @file array.h
+ * @brief Dynamic Array data structure public interface.
+ *
+ * This header defines the public interface for the Array data structure.
+ * It includes type definitions and function declarations.
+ */
+
 /*
  * Created by egor on 13.06.25. :)
  * Please note, this DS is an implementation of the Dynamic array with the active size and index
- * THis is made to avoid issue with the incorrect size sharing.
+ * This is made to avoid issue with the incorrect size sharing.
  *
  * But most of the functions in the toolbox always have the fixed size. So the Dynamic part is a precaution in case it will be necessary
  * For the same reason of very specific usage, the following Interface is not a full implementation of the Array logic, avoiding unnecessary functions
@@ -11,58 +19,155 @@
 #define ARRAY_H
 #include <stddef.h>
 
+/**
+ * @struct Array
+ * @brief Internal definition of the Dynamic Array structure.
+ * @ingroup Array
+ * @details
+ * This structure holds the necessary components for the dynamic array:
+ * It contains the pointer to the dynamically allocated data buffer,
+ * the current number of elements stored, and the total allocated capacity.
+ *
+ * @section ArrayStructDetails Detailed Structure Members
+ * This section provides a detailed description of each member of the `Array` struct.
+ *
+ * @var float* Array::data
+ * A pointer to the dynamically allocated block of `float` elements.
+ * This buffer holds the actual elements of the array.
+ *
+ * @var size_t Array::length
+ * The total allocated memory capacity of the array (number of elements).
+ * This value determines how many elements the `data` buffer can currently hold
+ * without requiring a reallocation.
+ *
+ * @var size_t Array::index
+ * The current number of elements actually stored and used in the array.
+ * This value represents the logical size of the array, indicating the next
+ * available position for new elements.
+ */
 typedef struct Array {
   float *data; // the array itself
   size_t length; // the length og the array
   size_t index; // the index of the next free cell of the array (used for inner size calculations)
 } Array;
 
-/*
- * This function is used to create new array
- * Input:
- *    const size_t length - the number of elements in the array
- * Output;
- *    The function returns the pointer to the structure of type Array
+/*!
+ * @ingroup ArrayLifecycle
+ * @brief Creates and initializes a new dynamic array.
+ * @param length The desired length of the array.
+ * @return A pointer to the new Array instance.
  */
-Array* ArrayCreate(const size_t length);
+Array* Array_Create(const size_t length);
 
-/*
- * This is used to concatenate two arrays
- * Input:
- *    const Array* array1 - the array to eb concatenated first
- *    const Array* array2 - the array to eb concatenated second
- * Output;
- *    The function returns the pointer to the structure of type Array
+/*!
+ * @ingroup ArrayLifecycle
+ * @brief Concatenates two arrays and initializes a new dynamic array.
+ * @param array1 the array to be concatenated.
+ * @param array2 the array to be concatenated.
+ * @return A pointer to the new Array instance.
  */
-Array* ArrayConcat(const Array* array1, const Array* array2);
+Array* Array_Concat(const Array* array1, const Array* array2);
 
-/*
- * This is used to destroy the struct Array
- * Input:
- *    Array *array - the array to be destroyed
- * Output;
- *    The function has no returns but manipulates a given pointer
+/*!
+ * @ingroup ArrayManipulation
+ * @brief Get and return a slice of the array data.
+ * @param array the array from which slice is made.
+ * @param start starting index of the slice.
+ * @param numberOfElements number of elements in the slice.
+ * @return A pointer to the new Array instance with the desired data.
+ * @note this was created to make the Matrix function be able to get rows
  */
-void ArrayDestroy(Array *array);
+Array* Array_ReturnDataFromTo(const Array *array, const size_t start, const size_t numberOfElements);
 
-/*
- * This is used to append new item to the array
- * Input:
- *    Array *array - the array to be destroyed
- * Output;
- *    The function has no returns but manipulates a given pointer
+/*!
+ * @ingroup ArrayLifecycle
+ * @brief Copy Array to a new Array pointer and return.
+ * @param arrayFrom the array from which copy will be made.
+ * @return A pointer to the new Array instance.
  */
-void ArrayAppend(Array *array, const float input);
+Array* Array_MakeCopy(const Array *arrayFrom);
 
-/*
- * This is used to append a list of items to the array
- * Input:
- *    Array *array - the array to be destroyed
- *    const float *input - the items to be appended
- *    const size_t pointerLength - size of the new data
- * Output;
- *    The function has no returns but manipulates a given pointer
+/*!
+ * @ingroup ArrayQuery
+ * @brief Return the specific index data from Array.
+ * @param array the array from which indexed value will be retrieved.
+ * @param index value of the index of desired value.
+ * @return A float value of the index.
  */
-void ArrayAppendPointer(Array *array, const float *input, const size_t pointerLength);
+float Array_GetCoordinate(const Array *array, const size_t index);
+
+/*!
+ * @ingroup ArrayManipulation
+ * @brief Set the specific index of the Array to the provided value.
+ * @param array the array which data will be altered.
+ * @param index value of the index of desired placement.
+ * @param value the value, which should be saved
+ */
+void Array_SetCoordinate(const Array *array, const size_t index, const float value);
+
+/*!
+ * @ingroup ArrayManipulation
+ * @brief Set several new values at specified position.
+ * @param array the array which will be manipulated.
+ * @param start starting index of the slice.
+ * @param numberOfElements number of elements in the slice.
+ * @param values the float type pointer to the data to be added.
+ */
+void Array_SetDataFromTo(const Array *array, const size_t start, const size_t numberOfElements, const float *values);
+
+/*!
+ * @ingroup ArrayLifecycle
+ * @brief Destroy the pointer of the struct Array.
+ * @param array the array which will be destroyed.
+ */
+void Array_Destroy(Array *array);
+
+/*!
+ * @ingroup ArrayManipulation
+ * @brief Append new value at the end of the Array.
+ * @param array the array to which the value will be added.
+ * @param value the value, which will be appended
+ */
+void Array_Append(Array *array, const float value);
+
+/*!
+ * @ingroup ArrayManipulation
+ * @brief Append array of elmeents of type float to the Array.
+ * @param array the array to which the values will be added.
+ * @param values the pointer to array of float values, which will be appended
+ * @param pointerLength the length of the given pointer of new values
+ */
+void Array_AppendPointer(Array *array, const float *values, const size_t pointerLength);
 
 #endif //ARRAY_H
+
+
+/**
+* @defgroup Array Array
+* @ingroup DataStructures
+* @brief Functions of the Data Structure Array.
+*/
+
+/**
+* @defgroup ArrayLifecycle Array Lifecycle
+* @ingroup Array
+* @brief Livsycle functions of the Array.
+*
+* This functions create/destroy/copy Array
+*/
+
+/**
+* @defgroup ArrayQuery Array Query
+* @ingroup Array
+* @brief Query of the Data Structure Array.
+*
+* This functions make read-only operations on data
+*/
+
+/**
+* @defgroup ArrayManipulation Array Manipulation
+* @ingroup Array
+* @brief Manipulation of the Data Structure Array.
+*
+* This functions make write operations on data
+*/
--- src/toolbox/data_structures/array.c
+++ src/toolbox/data_structures/array.c
@@ -9,20 +9,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*
- * This function is used to implement the size doubling. The famous Dynamic part of the interface
- * Input:
- *    Array *array - the array to be doubled
- * Output;
- *    The function has no return but manipulates a pointer
+#include "matrix.h"
+
+/*!
+ * @ingroup ArrayLifecycle
+ * @brief Double the size of the array.
+ * @param array the array to be doubled.
  */
-static void ArrayDouble(Array *array){
+static void Array_Double(Array *array){
   assert(array != NULL && "The array pointer can be NULL!");
 
   if (array->length == 0) {array->length = 1;}
 
   float *new_data_ptr = realloc(array->data, array->length * 2 * sizeof(float));
-  if (new_data_ptr == NULL){ perror("realloc command failed"); ArrayDestroy(array); exit(EXIT_FAILURE); }
+  if (new_data_ptr == NULL){ perror("realloc command failed"); Array_Destroy(array); exit(EXIT_FAILURE); }
 
   array->data = new_data_ptr;
   array->length = array->length * 2;
@@ -30,7 +30,7 @@ static void ArrayDouble(Array *array){
   for(size_t i = array->index; i < array->length; i++) array->data[i] = 0.0f;
 }
 
-Array* ArrayCreate(const size_t length){
+Array* Array_Create(const size_t length){
   assert(length >= 0 && "The size of array should be positive");
 
   Array *array = NULL;
@@ -47,12 +47,12 @@ Array* ArrayCreate(const size_t length){
   return array;
 }
 
-Array* ArrayConcat(const Array* array1, const Array* array2){
+Array* Array_Concat(const Array* array1, const Array* array2){
   assert(array1 != NULL && "The array1 can be NULL!");
   assert(array2 != NULL && "The array2 can be NULL!");
 
   const size_t newLength = array1->index + array2->index;
-  Array *newArray = ArrayCreate(newLength);
+  Array *newArray = Array_Create(newLength);
 
   if (array1->index > 0){ memcpy(newArray->data,                     array1->data, array1->index * sizeof(float)); }
   if (array2->index > 0){ memcpy(newArray->data + array1->index, array2->data, array2->index * sizeof(float)); }
@@ -60,25 +60,61 @@ Array* ArrayConcat(const Array* array1, const Array* array2){
   return newArray;
 }
 
-void ArrayAppend(Array *array, const float input){
+Array* Array_ReturnDataFromTo(const Array *array, const size_t start, const size_t numberOfElements){
+  Array* output = Array_Create(numberOfElements);
+  float *data = malloc((numberOfElements) * sizeof(float));
+  memcpy(data, array->data + start, numberOfElements * sizeof(float));
+
+  Array_AppendPointer(output, data, numberOfElements);
+  free(data);
+
+  return output;
+}
+
+Array* Array_MakeCopy(const Array *arrayFrom){
+  Array *output = Array_Create(arrayFrom->length);
+  memcpy(output->data, arrayFrom->data, arrayFrom->index * sizeof(float));
+  return output;
+}
+
+float Array_GetCoordinate(const Array *array, const size_t index){
+  return array->data[index];
+}
+
+void Array_SetCoordinate(const Array *array, const size_t index, const float value){
+  array->data[index] = value;
+}
+
+void Array_SetDataFromTo(const Array *array, const size_t start, const size_t numberOfElements, const float *values){
+  assert(array != NULL && "The array pointer can be NULL!");
+  assert(array->data != NULL && "The array data can be NULL!");
+  assert(values != NULL && "The values can be NULL!");
+
+  assert(numberOfElements > 0 && "Number of elements should more than 0!");
+  assert(start + numberOfElements <= array->length && "Start index out of range!");
+
+  memcpy(array->data + start, values, numberOfElements * sizeof(float));
+}
+
+void Array_Append(Array *array, const float value){
   assert(array != NULL && "The array can be NULL!");
   assert(array->data != NULL && "The array data can be NULL!");
 
-  if (array->index + 1 >= array->length){ ArrayDouble(array); }
+  if (array->index + 1 >= array->length){ Array_Double(array); }
   for(size_t i = 0; i < array->length; i++) printf("%f ", array->data[i]);
 
-  array->data[array->index] = input;
+  array->data[array->index] = value;
   array->index++;
 }
 
-void ArrayAppendPointer(Array *array, const float *input, const size_t pointerLength){
+void Array_AppendPointer(Array *array, const float *values, const size_t pointerLength){
   assert(array != NULL && "The array can be NULL!");
   assert(array->data != NULL && "The array data can be NULL!");
-  assert(input != NULL && "The input can be NULL!");
+  assert(values != NULL && "The input can be NULL!");
 
-  if (array->index + pointerLength - 1 >= array->length){ ArrayDouble(array); }
+  if (array->index + pointerLength - 1 >= array->length){ Array_Double(array); }
 
-  memcpy(array->data + array->index, input, pointerLength * sizeof(float));
+  memcpy(array->data + array->index, values, pointerLength * sizeof(float));
   array->index += pointerLength;
 }
 
