e07b013a7daaff4f81c7bfe8e0554d74d0e9137d
DemianovE <work.demianov@gmail.com>
2025-06-17 18:22:31 +0200

    The change of Matrix structure 1) add docs for matrix 2) spited functions by actions 3) changes to naming convention

--- include/toolbox/data_structures/matrix.h
+++ include/toolbox/data_structures/matrix.h
@@ -1,103 +1,191 @@
+/**
+ * @file matrix.h
+ * @brief Matrix data structure public interface.
+ *
+ * This header defines the public interface for the Matrix data structure.
+ * It includes type definitions and function declarations.
+ */
+
 #ifndef MATRIX_H
 #define MATRIX_H
 
-typedef struct Matrix{
-    float** matrix;
+#include "array.h"
 
-    int rows;
-    int cols;
+/**
+ * @struct Matrix
+ * @brief Internal definition of the Matrix structure.
+ * @ingroup Matrix
+ * @details
+ * This structure holds the necessary components for the matrix:
+ * It contains the pointer to the array strucute and configuration number of columns and rows.
+ *
+ * @section MatrixStructDetails Detailed Structure Members
+ * This section provides a detailed description of each member of the `Matrix` struct.
+ *
+ * @var Array* Array::matrix
+ * A pointer to the dynamically allocated structure Array.
+ * This buffer holds the Array which is responsible for actual data
+ *
+ * @var size_t Array::rows
+ * The total number of rows in the Matrix
+ * This value is used in the 1D array to 2D Matrix conversion as well as configurations
+ *
+ * @var size_t Array::cols
+ * The total number of cols in the Matrix
+ * This value is used in the 1D array to 2D Matrix conversion as well as configurations
+ */
+typedef struct Matrix{
+    Array *matrix; // the matrix array itself
+
+    size_t rows; // the number of rows
+    size_t cols; // the number of columns
 } Matrix;
 
-/*
- * This function is used to create new matrix
- * Input:
- *    const int rows - the number of rows in new matrix
- *    const int cols - the number of columns in new matrix
- * Output;
- *    The function returns the pointer to the structure of type Matrix
- */
-Matrix* MatrixCreate(const int rows, const int cols);
 
-/*
- * This function is used to create matrix from values of a pointer array "input"
- * Input:
- *    const float *input - the float array pointer which contains values for the matrix
- *    const int rows - the number of rows in new matrix
- *    const int cols - the number of columns in new matrix
- * Output;
- *    The function returns the pointer to the structure of type Matrix
- */
-Matrix* MatrixCreateFromPointer(const float *input, const int rows, const int cols);
 
-/*
- * This function is used to multiply two matrices in form A * B
- * Input:
- *    const Matrix *leftMatrix - the left matrix of the action
- *    const Matrix *rightMatrix - the right matrix of the action
- * Output;
- *    The function returns the pointer to the structure of type Matrix
- */
-Matrix* MatrixMultiply(const Matrix *leftMatrix, const Matrix *rightMatrix);
+//=============================================================================
+//
+//                     Matrix Lifecycle Management Functions
+//
+//=============================================================================
 
-/*
- * This function is used to add/substruct two matrices in format A-B or A+B
- * Input:
- *    const Matrix *leftMatrix - the left matrix of the action
- *    const Matrix *rightMatrix - the right matrix of the action
- *    const int type - the definer of the type of actions made. 0 - subs, 1 - add
- * Output;
- *    The function returns the pointer to the structure of type Matrix
+/*!
+ * @ingroup MatrixLifecycle
+ * @brief Creates and initializes a new matrix.
+ * @param rows the desired number of rows.
+ * @param cols the desired number of columns.
+ * @return A pointer to the new Matrix instance.
  */
-Matrix* MatrixSubstAdd(const Matrix *leftMatrix, const Matrix *rightMatrix, const int type);
+Matrix* Matrix_Create(const size_t rows, const size_t cols);
 
-/*
- * This function is used to make a copy of the matrix provided
- * Input:
- *    const Matrix *matrix - the matrix which will be copied
- * Output;
- *    The function returns the pointer to the structure of type Matrix
+/*!
+ * @ingroup MatrixLifecycle
+ * @brief Creates and initializes a new matrix as well as populate it with values from the float tape pointer.
+ * @param input the type float pointer with data to be used
+ * @param rows the desired number of rows.
+ * @param cols the desired number of columns.
+ * @return A pointer to the new Matrix instance.
  */
-Matrix* MatrixMakeCopy(const Matrix *input);
+Matrix* Matrix_CreateFromPointer(const float *input, const size_t rows, const size_t cols);
 
-/*
- * This function is used to pass all values trough function
- * Input:
- *    const Matrix *matrix - the pointer to struct matrix which is used for the action
- *    float (*func_ptr)(float) - the function pointer for the action
- * Output;
- *    The function returns nothing. But makes direct changes to the matrix pointer provided
+/*!
+ * @ingroup MatrixLifecycle
+ * @brief Make and initialize a copy of the matrix provided.
+ * @param source the matrix pointer to be copied
+ * @return A pointer to the new Matrix instance.
  */
-void MatrixApplyFormula(const Matrix *matrix, float (*func_ptr)(float));
+Matrix* Matrix_MakeCopy(const Matrix *source);
 
-/*
- * This function is used to fully delete matrix
- * Input:
- *    const Matrix *matrix - the pointer to struct matrix which is used for the action
- * Output;
- *    The function returns nothing.
+/*!
+ * @ingroup MatrixLifecycle
+ * @brief Destroy matrix pointer.
+ * @param matrix the matrix to be destroyed
  */
-void MatrixDelete(Matrix *matrix);
+void Matrix_Destroy(Matrix *matrix);
 
-/*
- * This function is used to delete the data of the matrix
- * Input:
- *    const Matrix *matrix - the pointer to struct matrix which is used for the action
- * Output;
- *    The function returns nothing. But makes direct changes to the matrix pointer provided
+
+
+//=============================================================================
+//
+//                     Matrix Query Functions
+//
+//=============================================================================
+
+/*!
+ * @ingroup MatrixQuery
+ * @brief Return the values of the matrix as specific 2D coordinate from 1D array.
+ * @param matrix the matrix which value will be returned
+ * @param row the desired row.
+ * @param col the desired column.
+ * @return A float value at the coordinate from matrix
  */
-void MatrixClear(Matrix *matrix);
+float Matrix_GetCoordinate(const Matrix *matrix, const size_t row, const size_t col);
 
-/*
- * This function is used to reshape matrix size
- * Input:
- *    const Matrix *matrix - the pointer to struct matrix which is used for the action
- *    const int rows - new matrix number of the rows
- *    const int cols - new matrix number of the column
- * Output;
- *    The function returns nothing. But makes direct changes to the matrix pointer provided
+/*!
+ * @ingroup MatrixQuery
+ * @brief Return the type float pointer of the matrix with values of one row.
+ * @param matrix the matrix which value will be returned
+ * @param row the desired row.
+ * @return An Array instance with the row objects
  */
+Array* Matrix_GetRow(const Matrix *matrix, const size_t row);
 
-void MatrixReshape(Matrix *matrix, const int rows, const int cols);
+//=============================================================================
+//
+//                     Matrix Manipulation Functions
+//
+//=============================================================================
+
+/*!
+ * @ingroup MatrixManipulation
+ * @brief Delete the data of the matrix.
+ * @param matrix the matrix which data is to be deleted
+ */
+void Matrix_Delete(Matrix *matrix);
+
+/*!
+ * @ingroup MatrixManipulation
+ * @brief Set the value in the matrix at specific coordinated to the provided.
+ * @param matrix the matrix which data is to be modified
+ * @param row the desired row.
+ * @param col the desired column.
+ * @param value the value to be set.
+ */
+void Matrix_SetCoordinate(const Matrix *matrix, const size_t row, const size_t col, const float value);
+
+/*!
+ * @ingroup MatrixManipulation
+ * @brief Apply a function to all values of the matrix.
+ * @param matrix the matrix which data is to be modified
+ * @param func_ptr the function to be applied to all values.
+ */
+void Matrix_ApplyFormula(const Matrix *matrix, float (*func_ptr)(float));
+
+/*!
+ * @ingroup MatrixManipulation
+ * @brief Change the dimensions of the matrix.
+ * @param matrix the matrix which data is to be modified
+ * @param rows the desired new row count.
+ * @param cols the desired new column count.
+ *
+ * @note Historically, when matrix was float** the reshaping was O(n^2) change to the new matrix. Now it is just a change of variable for index formula
+ */
+void Matrix_Reshape(Matrix *matrix, const size_t rows, const size_t cols);
+
+
+
+//=============================================================================
+//
+//                     Matrix Math Functions
+//
+//=============================================================================
+
+/*!
+ * @ingroup MatrixManipulation
+ * @brief Perform matrix multiplication and return the resulting initialized matrix instance.
+ * @param leftMatrix the left matrix of the multiplication.
+ * @param rightMatrix the right matrix of the multiplication.
+ * @return A pointer to the new Matrix instance.
+ */
+Matrix* Matrix_Multiply(const Matrix *leftMatrix, const Matrix *rightMatrix);
+
+/*!
+ * @ingroup MatrixManipulation
+ * @brief Perform matrix addition and return the resulting initialized matrix instance.
+ * @param leftMatrix the left matrix of the multiplication.
+ * @param rightMatrix the right matrix of the multiplication.
+ * @return A pointer to the new Matrix instance.
+ */
+Matrix* Matrix_Add(const Matrix *leftMatrix, const Matrix *rightMatrix);
+
+/*!
+ * @ingroup MatrixManipulation
+ * @brief Perform matrix subtraction and return the resulting initialized matrix instance.
+ * @param leftMatrix the left matrix of the multiplication.
+ * @param rightMatrix the right matrix of the multiplication.
+ * @return A pointer to the new Matrix instance.
+ */
+Matrix* Matrix_Subs(const Matrix *leftMatrix, const Matrix *rightMatrix);
 
 #define CLEAR_MATRIX_UNTIL(matrix, count_to_free) do { \
     if (matrix != NULL) {                              \
@@ -111,4 +199,43 @@ void MatrixReshape(Matrix *matrix, const int rows, const int cols);
 } while (0)
 
 #endif
- 
+
+/**
+* @defgroup Matrix Matrix
+* @ingroup DataStructures
+* @brief Functions of the Data Structure Array.
+*/
+
+/**
+* @defgroup MatrixLifecycle Matrix Lifecycle
+* @ingroup Matrix
+* @brief Lifecycle functions of the Matrix.
+*
+* This functions create/destroy/copy Matrix
+*/
+
+/**
+* @defgroup MatrixQuery Matrix Query
+* @ingroup Matrix
+* @brief Query of the Data Structure Matrix.
+*
+* This functions make read-only operations on data
+*/
+
+/**
+* @defgroup MatrixManipulation Matrix Manipulation
+* @ingroup Matrix
+* @brief Manipulation of the Data Structure Matrix.
+*
+* This functions make write operations on data
+*/
+
+/**
+* @defgroup MatrixMath Matrix Math
+* @ingroup Matrix
+* @brief Math Operations of the Data Structure Matrix.
+*
+* This functions perform math operations
+*
+* @note this will be seperated into a separate file as it is not directly interface operations
+*/
--- src/toolbox/data_structures/matrix.c
+++ src/toolbox/data_structures/matrix.c
@@ -1,3 +1,10 @@
+/**
+ * @file matrix.c
+ * @brief Matrix data structure public interface implementation.
+ *
+ * This file defines all implementations of the Matrix public interface
+ */
+
 #include "matrix.h"
 
 #include <assert.h>
@@ -5,57 +12,140 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
- * This function is used to creat a matrix grid from params
- * Input:
- *    const Matrix *matrix - the pointer to struct matrix which is used for the action
- * Output;
- *    The function returns nothing. But makes direct changes to the matrix pointer provided
- */
-static void MatrixCreateGrid(Matrix *matrix){
-    matrix->matrix = malloc(matrix->rows * sizeof(float*));
-    if (matrix->matrix == NULL) { perror("Failed to allocate Matrix rows"); free(matrix); exit(EXIT_FAILURE); }
+//=============================================================================
+//
+//                     Matrix Lifecycle Management Functions
+//
+//=============================================================================
 
-    for(int i=0; i<matrix->rows; i++){
-        matrix->matrix[i] = NULL;
-        matrix->matrix[i] = malloc(matrix->cols * sizeof(float));
-        if (matrix->matrix[i] == NULL) { perror("Failed to allocate Matrix column"); CLEAR_MATRIX_UNTIL(matrix->matrix, i); free(matrix); exit(EXIT_FAILURE); }
-    }
-}
-
-Matrix* MatrixCreate(const int rows, const int cols){
+Matrix* Matrix_Create(const size_t rows, const size_t cols){
     /* Matrix *matrix - the output matrix pointer */
-
     assert(rows > 0 && "array of sizes value x should be positive!");
     assert(cols > 0 && "array of sizes value y should be positive!");
 
     Matrix *matrix = NULL;
-    matrix = malloc(sizeof(Matrix));
+    matrix = malloc(sizeof(Array));
     if (matrix == NULL){ perror("Failed to allocate Matrix"); exit(EXIT_FAILURE); }
 
     matrix->rows = rows;
     matrix->cols = cols;
-    matrix->matrix = NULL;
 
-    MatrixCreateGrid(matrix);
+    matrix->matrix = NULL;
+    matrix->matrix = Array_Create(matrix->rows*matrix->cols);
 
     return matrix;
 }
 
-Matrix* MatrixCreateFromPointer(const float *input, const int rows, const int cols){
+Matrix* Matrix_CreateFromPointer(const float *input, const size_t rows, const size_t cols){
     /* Matrix *matrix - the output matrix pointer */
-
     assert(input != NULL && "input pointer should not be NULL!");
 
     // sizes are in format [row, col]
-    Matrix *output = MatrixCreate(1, rows * cols);
-    memcpy(output->matrix[0], input, sizeof(float) * output->cols * output->rows);
-
-    MatrixReshape(output, rows, cols);
+    Matrix *output = Matrix_Create(rows, cols);
+    Array_AppendPointer(output->matrix, input, rows*cols);
     return output;
 }
 
-Matrix* MatrixMultiply(const Matrix *leftMatrix, const Matrix *rightMatrix){
+Matrix* MatrixMakeCopy(const Matrix *input){
+    /* Matrix *matrix - the output matrix pointer */
+    Matrix *output = Matrix_Create(input->rows, input->cols);
+
+    Array_AppendPointer(output->matrix, input->matrix->data, input->rows * input->cols);
+    return output;
+}
+
+void MatrixDestroy(Matrix *matrix){
+    if (matrix == NULL) { return; }
+
+    Matrix_Delete(matrix);
+    free(matrix);
+}
+
+
+
+//=============================================================================
+//
+//                     Matrix Query Functions
+//
+//=============================================================================
+
+float Matrix_GetCoordinate(const Matrix *matrix, const size_t row, const size_t col){
+    assert(matrix != NULL && "matrix pointer should not be NULL!");
+    assert(row >= 0 && "row index should be positive!");
+    assert(col >= 0 && "col index should be positive!");
+
+    assert(row <= matrix->rows && "row index is out of range!");
+    assert(col <= matrix->cols && "col index is out of range!");
+
+    return Array_GetCoordinate(matrix->matrix, row * matrix->cols + col);
+}
+
+Array* Matrix_GetRow(const Matrix *matrix, const size_t row){
+    assert(matrix != NULL && "matrix pointer should not be NULL!");
+    assert(row >= 0 && "row index should be positive!");
+
+    assert(row <= matrix->rows && "row index is out of range!");
+    return Array_ReturnDataFromTo(matrix->matrix, row*matrix->cols, matrix->cols);
+}
+
+
+
+
+//=============================================================================
+//
+//                     Matrix Manipulation Functions
+//
+//=============================================================================
+
+void Matrix_Delete(Matrix *matrix){
+    if (matrix == NULL || matrix->matrix == NULL) { return; }
+
+    Array_Destroy(matrix->matrix);
+    matrix->matrix = NULL;
+}
+
+
+void Matrix_SetCoordinate(const Matrix *matrix, const size_t row, const size_t col, const float value){
+    assert(matrix != NULL && "matrix pointer should not be NULL!");
+    assert(row >= 0 && "row index should be positive!");
+    assert(col >= 0 && "col index should be positive!");
+
+    assert(row <= matrix->rows && "row index is out of range!");
+    assert(col <= matrix->cols && "col index is out of range!");
+
+    Array_SetCoordinate(matrix->matrix, row * matrix->cols + col, value);
+}
+
+void Matrix_ApplyFormula(const Matrix *matrix, float (*func_ptr)(float)){
+    assert(matrix != NULL && "matrix pointer should not be NULL!");
+
+    for(int i=0; i<matrix->rows; i++){
+        for(int y=0; y<matrix->cols; y++){
+            const float newValue = func_ptr(Matrix_GetCoordinate(matrix, i, y));
+            Matrix_SetCoordinate(matrix, i, y, newValue);
+        }
+    }
+}
+
+void Matrix_Reshape(Matrix *matrix, const size_t rows, const size_t cols){
+    assert(rows >= 0 && "row index should be positive!");
+    assert(cols >= 0 && "col index should be positive!");
+
+    assert(rows + cols == matrix->rows + matrix->cols);
+
+    matrix->rows = rows;
+    matrix->cols = cols;
+}
+
+
+
+//=============================================================================
+//
+//                     Matrix Math Functions
+//
+//=============================================================================
+
+Matrix* Matrix_Multiply(const Matrix *leftMatrix, const Matrix *rightMatrix){
     /* Matrix *matrix - the output matrix pointer */
 
     assert(leftMatrix != NULL && "leftMatrix pointer should not be NULL!");
@@ -65,78 +155,47 @@ Matrix* MatrixMultiply(const Matrix *leftMatrix, const Matrix *rightMatrix){
         fprintf(stderr, "Error: Sizes of matrix's are incorrect\n");
         exit(EXIT_FAILURE);
     }
-    Matrix *output = MatrixCreate(leftMatrix->rows, rightMatrix->cols);
+    Matrix *output = Matrix_Create(leftMatrix->rows, rightMatrix->cols);
 
-    for(int i=0; i < rightMatrix->cols; i++){
+    for(size_t i=0; i < rightMatrix->cols; i++){
         // start of a column of matrix
-        for(int y=0; y < leftMatrix->rows; y++){
-            // start of a row of matrix 
-            output->matrix[y][i] = 0;
-            for(int x=0; x < leftMatrix->cols; x++) output->matrix[y][i] += leftMatrix->matrix[y][x] * rightMatrix->matrix[x][i];
+        for(size_t y=0; y < leftMatrix->rows; y++){
+            // start of a row of matrix
+
+            Matrix_SetCoordinate(output, y, i, 0);
+            for(size_t x=0; x < leftMatrix->cols; x++){
+                const float newValue = Matrix_GetCoordinate(rightMatrix, y, x) * Matrix_GetCoordinate(leftMatrix, x, i);
+                Matrix_SetCoordinate(output, y, i, newValue);
+            }
         }
     }
     return output;
 }
 
-Matrix* MatrixMakeCopy(const Matrix *input){
+static Matrix* Matrix_SubstAdd(const Matrix *leftMatrix, const Matrix *rightMatrix, const size_t type){
     /* Matrix *matrix - the output matrix pointer */
+    assert(leftMatrix != NULL && "leftMatrix pointer should not be NULL!");
+    assert(rightMatrix != NULL && "rightMatrix pointer should not be NULL!");
 
-    Matrix *output = MatrixCreate(input->rows, input->cols);
 
-    for(int i=0; i < output->rows; i++) memcpy(output->matrix[i], input->matrix[i], output->cols * sizeof(float));
+    Matrix *output = Matrix_Create(leftMatrix->rows, rightMatrix->cols);
+
+    for(size_t i=0; i<output->rows; i++){
+        for(size_t y=0; y<output->cols; y++){
+            const float leftValue = Matrix_GetCoordinate(leftMatrix, i, y);
+            const float rightValue = Matrix_GetCoordinate(rightMatrix, i, y);
+
+            const float newValue = (type == 0) ? leftValue - rightValue : leftValue + rightValue;
+            Matrix_SetCoordinate(output, i, y, newValue);
+        }
+    }
     return output;
 }
 
-Matrix* MatrixSubstAdd(const Matrix *leftMatrix, const Matrix *rightMatrix, const int type){
-    /* Matrix *matrix - the output matrix pointer */
-
-    Matrix *output = MatrixCreate(leftMatrix->rows, rightMatrix->cols);
-
-    for(int i=0; i<output->rows; i++){
-        for(int y=0; y<output->cols; y++) output->matrix[i][y] = (type == 0) ? leftMatrix->matrix[i][y] - rightMatrix->matrix[i][y] : leftMatrix->matrix[i][y] + rightMatrix->matrix[i][y];
-    }
-    return output;
+Matrix* Matrix_Add(const Matrix *leftMatrix, const Matrix *rightMatrix){
+    return Matrix_SubstAdd(leftMatrix, rightMatrix, 1);
 }
 
-void MatrixReshape(Matrix *matrix, const int rows, const int cols){
-    /*  Matrix *copy - the matrix pointer used to story a copy of matrix shaped */
-    Matrix *copy = MatrixMakeCopy(matrix);
-
-    MatrixClear(matrix);
-    matrix->rows = rows;
-    matrix->cols = cols;
-
-    MatrixCreateGrid(matrix);
-
-    int copyX = 0, copyY = 0;
-    int mainX = 0, mainY = 0;
-
-    for (size_t j=0; j<matrix->cols * matrix->rows; j++){
-        matrix->matrix[mainX][mainY] = copy->matrix[copyX][copyY];
-
-        mainY++; copyY++;
-        if (mainY == matrix->cols) {mainY = 0; mainX++;};
-        if (copyY == copy->cols) {copyY = 0; copyX++;};
-    }
-
-    MatrixDelete(copy);
-}
-
-void MatrixClear(Matrix *matrix){
-    if(matrix){
-        for(int i=0; i<matrix->rows; i++) free(matrix->matrix[i]);
-        free(matrix->matrix);
-        matrix->matrix = NULL;
-    }
-}
-
-void MatrixDelete(Matrix *matrix){
-    MatrixClear(matrix);
-    free(matrix);
-}
-
-void MatrixApplyFormula(const Matrix *matrix, float (*func_ptr)(float)){
-    for(int i=0; i<matrix->rows; i++){
-        for(int y=0; y<matrix->cols; y++) matrix->matrix[i][y] = func_ptr(matrix->matrix[i][y]);
-    }
+Matrix* Matrix_Subs(const Matrix *leftMatrix, const Matrix *rightMatrix){
+    return Matrix_SubstAdd(leftMatrix, rightMatrix, 0);
 }
