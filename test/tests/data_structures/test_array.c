3836d6340f9a8d2a04a2912c6c9805ba83d975e3
DemianovE <work.demianov@gmail.com>
2025-06-26 15:40:45 +0200

    change of tests to float 1) make the array test ArrayFloat

--- test/tests/data_structures/test_array.c
+++ test/tests/data_structures/test_array.c
@@ -1,79 +1,79 @@
 //
 // Created by egor on 16.06.25.
 //
-#include "array.h"
+#include "array_float.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 #include "unity/unity.h"
 
-Array *usedArrayOne;
-Array *usedArrayTwo;
+ArrayFloat *usedArrayOne;
+ArrayFloat *usedArrayTwo;
 
 void setUp(void){
   const float values[] = {2.0f, 3.0f};
 
-  usedArrayOne = Array_Create(2);
-  usedArrayTwo = Array_Create(2);
+  usedArrayOne = ArrayFloat_Create(2);
+  usedArrayTwo = ArrayFloat_Create(2);
 
-  Array_AppendPointer(usedArrayOne, values, 2);
-  Array_AppendPointer(usedArrayTwo, values, 2);
+  ArrayFloat_AppendPointer(usedArrayOne, values, 2);
+  ArrayFloat_AppendPointer(usedArrayTwo, values, 2);
 }
 
 void tearDown(void){
-  Array_Destroy(usedArrayOne);
-  Array_Destroy(usedArrayTwo);
+  ArrayFloat_Destroy(usedArrayOne);
+  ArrayFloat_Destroy(usedArrayTwo);
 }
 
 
 void testArray_Concat(void){
-  Array *newArray = Array_Concat(usedArrayOne, usedArrayTwo);
+  ArrayFloat *newArray = ArrayFloat_Concat(usedArrayOne, usedArrayTwo);
   const float correct[] = {2.0f, 3.0f, 2.0f, 3.0f};
 
-  TEST_ASSERT_EQUAL_FLOAT_ARRAY(correct, Array_GetArray(newArray), 4);
+  TEST_ASSERT_EQUAL_FLOAT_ARRAY(correct, ArrayFloat_GetArray(newArray), 4);
 
-  Array_Destroy(newArray);
+  ArrayFloat_Destroy(newArray);
 }
 
 void testArray_Append(void){
-  Array_Append(usedArrayOne, 2.0f);
+  ArrayFloat_Append(usedArrayOne, 2.0f);
   const float correct[] = {2.0f, 3.0f, 2.0f};
 
-  TEST_ASSERT_EQUAL_FLOAT_ARRAY(correct, Array_GetArray(usedArrayOne), 3);
-  TEST_ASSERT_EQUAL(Array_GetLength(usedArrayOne), 4);
-  TEST_ASSERT_EQUAL(Array_GetIndex(usedArrayOne), 3);
+  TEST_ASSERT_EQUAL_FLOAT_ARRAY(correct, ArrayFloat_GetArray(usedArrayOne), 3);
+  TEST_ASSERT_EQUAL(ArrayFloat_GetCapacity(usedArrayOne), 4);
+  TEST_ASSERT_EQUAL(ArrayFloat_GetIndex(usedArrayOne), 3);
 }
 
 void testArray_SetCoordinate(void){
-  Array_SetCoordinate(usedArrayOne, 0, 4.0f);
+  ArrayFloat_SetCoordinate(usedArrayOne, 0, 4.0f);
   const float correct[] = {4.0f, 3.0f};
 
-  TEST_ASSERT_EQUAL_FLOAT_ARRAY(Array_GetArray(usedArrayOne), correct, 2);
+  TEST_ASSERT_EQUAL_FLOAT_ARRAY(ArrayFloat_GetArray(usedArrayOne), correct, 2);
 }
 
 void testArray_SetDataFromTo(void){
   const float data[] = {5.0f, 6.0f};
-  Array_SetDataFromTo(usedArrayOne, 0, 2, data);
+  ArrayFloat_SetDataFromTo(usedArrayOne, 0, 2, data);
 
-  TEST_ASSERT_EQUAL_FLOAT_ARRAY(Array_GetArray(usedArrayOne), data, 2);
+  TEST_ASSERT_EQUAL_FLOAT_ARRAY(ArrayFloat_GetArray(usedArrayOne), data, 2);
 }
 
 void testArray_Copy(void){
-  Array *copyArray = Array_MakeCopy(usedArrayOne);
-  TEST_ASSERT_TRUE(Array_CheckEqual(copyArray, usedArrayOne));
-  Array_Destroy(copyArray);
+  ArrayFloat *copyArray = ArrayFloat_MakeCopy(usedArrayOne);
+  TEST_ASSERT_TRUE(ArrayFloat_CheckEqual(copyArray, usedArrayOne));
+  ArrayFloat_Destroy(copyArray);
 }
 
 void testArray_Getters(void){
   const float values[] = {2.0f, 3.0f};
 
-  TEST_ASSERT_EQUAL(Array_GetIndex(usedArrayOne), 2);
-  TEST_ASSERT_EQUAL(Array_GetLength(usedArrayOne), 2);
+  TEST_ASSERT_EQUAL(ArrayFloat_GetIndex(usedArrayOne), 2);
+  TEST_ASSERT_EQUAL(ArrayFloat_GetCapacity(usedArrayOne), 2);
 
-  TEST_ASSERT_EQUAL(Array_GetValue(usedArrayOne, 0), 2.0f);
-  TEST_ASSERT_EQUAL(Array_GetValue(usedArrayOne, 1), 3.0f);
+  TEST_ASSERT_EQUAL(ArrayFloat_GetValue(usedArrayOne, 0), 2.0f);
+  TEST_ASSERT_EQUAL(ArrayFloat_GetValue(usedArrayOne, 1), 3.0f);
 
-  TEST_ASSERT_EQUAL_FLOAT_ARRAY(Array_GetArray(usedArrayOne), values, 2);
+  TEST_ASSERT_EQUAL_FLOAT_ARRAY(ArrayFloat_GetArray(usedArrayOne), values, 2);
 }
 
 int main(void){
